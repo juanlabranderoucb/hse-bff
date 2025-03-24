e0af63551bc5effd87d5f27c751f70507b665072
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-03-23 20:15:40 -0400

    chore: first seeder

---
+++ src/seeders/demo.seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.createMany({
+    data: [
+      {
+        userName: 'admin',
+        email: 'admin@company.com',
+        password: await hash('company', 12),
+      },
+    ],
+  });
+
+  console.log('Datos sembrados exitosamente.');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
