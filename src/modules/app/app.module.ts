fbc38011692eb777ebc61c4d90745061b8752f32
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-03-23 20:17:10 -0400

    chore: auth and database module integration

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { config } from 'src/config';
+import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,6 +15,8 @@ import { AppService } from './app.service';
       load: [config],
       expandVariables: true,
     }),
+    DatabaseModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
