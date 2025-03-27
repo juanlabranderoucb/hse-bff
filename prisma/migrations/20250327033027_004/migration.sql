671d93380205f4b3189dd9acf26b0ad2793654ed
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-03-27 00:47:31 -0400

    chore: db migrations

---
+++ prisma/migrations/20250327033027_004/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "SubstandardConditionReport" ALTER COLUMN "date" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "SubstandardConditionReportLike" ALTER COLUMN "date" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "SubstandardConditionReportReply" ALTER COLUMN "date" SET DEFAULT CURRENT_TIMESTAMP;
