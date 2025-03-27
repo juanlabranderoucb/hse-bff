78b5b4538dd6e1210c10ca7e17727c696d24e904
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-03-27 18:25:46 -0400

    feat: sc-report impact services

---
+++ src/modules/safety/sc-report/dto/create-sc-report-impact.dto.ts
@@ -0,0 +1,26 @@
+import { Expose, Transform } from 'class-transformer';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateScReportImpactDto {
+  @Expose()
+  @IsNotEmpty()
+  @IsDefined()
+  @IsNumber()
+  substandardConditionReportId: number;
+
+  @Expose()
+  @IsNotEmpty()
+  @IsDefined()
+  @IsString()
+  @MinLength(16)
+  @Transform(({ value }: { value: string }) =>
+    value ? value.toString().trim() : value,
+  )
+  description: string;
+}
