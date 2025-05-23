29e37a3eed83d32e4d35546e1702f384ab1541f4
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-05-22 23:47:03 -0400

    feat: substandard condition entity

---
+++ src/modules/safety/substandard-conditions/domain/entities/substandard-condition.entity.ts
@@ -0,0 +1,8 @@
+export class SubstandardCondition {
+  constructor(
+    public readonly id: number,
+    public description: string,
+    public location: string,
+    public date: Date,
+  ) {}
+}
